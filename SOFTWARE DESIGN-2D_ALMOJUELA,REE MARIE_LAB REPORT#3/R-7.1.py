83a8486dbe7c053bde45850d3cf58bff91614b8a
eirameer <102618578+eirameer@users.noreply.github.com>
2022-06-01 16:09:42 +0800

    Add files via upload

---
+++ SOFTWARE DESIGN-2D_ALMOJUELA,REE MARIE_LAB REPORT#3/R-7.1.py
@@ -0,0 +1,29 @@
+ # Creating Node class Node:
+ import self as self
+
+
+ def __init__(self, x):
+self.val = x
+self.next = None
+# Fucntion to insert new node at front
+def push(head, n_val):
+t = Node(n_val)
+t.next = head
+head = t
+return head
+# Creating LL
+head = None
+head = push(head, 1)
+head = push(head, 2)
+head = push(head, 3)
+head = push(head, 4)
+head = push(head, 5)
+# 5 -> 4 -> 3 -> 2 -> 1 -> None
+# Algorithm temp = head
+# check if list is empty if (temp == None or temp.next == None):
+print("List empty")
+while (temp != None):
+if (temp.next.next == None):
+print(temp.val)
+break
+temp = temp.next
